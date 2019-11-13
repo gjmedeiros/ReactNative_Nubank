cc289c6bd5c98c87dc59a50256e7ab881f340d8d
Medeiros Goncalves <gjmedeiros@indracompany.com>
2019-11-13 12:04:31 -0300

    Projeto Finalizado

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -21,6 +21,7 @@ import {
 } from './styles';
 
 export default function Main() {
+  let offset = 0;
   const translateY = new Animated.Value(0);
 
   const animatedEvent = Animated.event(
@@ -33,14 +34,39 @@ export default function Main() {
     ],
     {useNativeDriver: true},
   );
-  function onHandlerStateChanged(event) {}
+  function onHandlerStateChanged(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const {translationY} = event.nativeEvent;
+
+      offset += translationY;
+
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
+  }
 
   return (
     <Container>
       <Header />
 
       <Content>
-        <Menu />
+        <Menu translateY={translateY} />
 
         <PanGestureHandler
           onGestureEvent={animatedEvent}
@@ -49,7 +75,11 @@ export default function Main() {
             style={{
               transform: [
                 {
-                  translateY,
+                  translateY: translateY.interpolate({
+                    inputRange: [-350, 0, 380],
+                    outputRange: [-50, 0, 380],
+                    extrapolate: 'clamp',
+                  }),
                 },
               ],
             }}>
@@ -71,7 +101,7 @@ export default function Main() {
         </PanGestureHandler>
       </Content>
 
-      <Tabs />
+      <Tabs translateY={translateY} />
     </Container>
   );
 }
