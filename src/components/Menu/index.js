ffb1e4fa1202b1e463c1c30c6f9c99778ba4f112
Medeiros Goncalves <gjmedeiros@indracompany.com>
2019-11-13 09:47:10 -0300

    Menu completo e tentativa de deixar pagina dinamica

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,19 +1,54 @@
 import React from 'react';
-import QRCode from 'react-native-qrcode-image';
+import QRCode from 'react-native-qrcode-svg';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {Container, Code} from './styles';
+import {
+  Container,
+  Code,
+  Nav,
+  NavItem,
+  NavText,
+  SignOutButton,
+  SignOutButtonText,
+} from './styles';
 
 export default function Menu() {
   return (
     <Container>
       <Code>
         <QRCode
-          value="https://rocketseat.com.br"
+          value="https://rocketseat.com.br/"
           size={80}
-          bgColor="#FFFFFF"
-          fgColor="#8B10AE"
+          fgColor="#FFF"
+          bgColor="#8B10AE"
         />
       </Code>
+
+      <Nav>
+        <NavItem>
+          <Icon name="help-outline" size={20} color="#fff" />
+          <NavText>Me ajuda</NavText>
+        </NavItem>
+
+        <NavItem>
+          <Icon name="person-outline" size={20} color="#fff" />
+          <NavText>Perfil</NavText>
+        </NavItem>
+
+        <NavItem>
+          <Icon name="credit-card" size={20} color="#fff" />
+          <NavText>Configurar cartão</NavText>
+        </NavItem>
+
+        <NavItem>
+          <Icon name="smartphone" size={20} color="#fff" />
+          <NavText>Configurações do app</NavText>
+        </NavItem>
+      </Nav>
+
+      <SignOutButton onPress={() => {}}>
+        <SignOutButtonText>SAIR DO APP</SignOutButtonText>
+      </SignOutButton>
     </Container>
   );
 }
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import {Animated} from 'react-native';
+import {PanGestureHandler, State} from 'react-native-gesture-handler';
+
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
-// import Menu from '~/components/Menu';
+import Menu from '~/components/Menu';
 
 import {
   Container,
@@ -18,29 +21,54 @@ import {
 } from './styles';
 
 export default function Main() {
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        },
+      },
+    ],
+    {useNativeDriver: true},
+  );
+  function onHandlerStateChanged(event) {}
+
   return (
     <Container>
       <Header />
 
       <Content>
-        {/* <Menu /> */}
+        <Menu />
 
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
-          <CardContent>
-            <Title>Saldo disponivel</Title>
-            <Description>R$ 197.611,69</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Transferência de R$ 20,00 recebia de Gabriel Medeiros Gonçalves
-              hoje às 18:00h
-            </Annotation>
-          </CardFooter>
-        </Card>
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChanged}>
+          <Card
+            style={{
+              transform: [
+                {
+                  translateY,
+                },
+              ],
+            }}>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponivel</Title>
+              <Description>R$ 197.611,69</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 20,00 recebia de Gabriel Medeiros Gonçalves
+                hoje às 18:00h
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
 
       <Tabs />
